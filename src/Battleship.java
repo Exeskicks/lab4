27aecc5baa85b1b14478422b0fcc08eb6353ba2f
Yudin Danil <danil.yudin.2014@list.ru>
2022-07-07 16:09:41 +0700

    Added drawField implementation

--- src/Battleship.java
+++ src/Battleship.java
@@ -3,7 +3,19 @@ public class Battleship {
 
 
     public int drawField(int[][] battlefield) {
-        //todo реализовать метод drawField позднее до конца
-        return 10;
+        int i = 0;
+        System.out.println("  0 1 2 3 4 5 6 7 8 9");
+        for (; i < battlefield.length; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < battlefield[1].length; j++) {
+                if (battlefield[j][i] == 0) {
+                    System.out.print("- ");
+                } else {
+                    System.out.print("X ");
+                }
+            }
+            System.out.println();
+        }
+        return i;
     }
 }
