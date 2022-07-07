a94461000a9011c2fdfb02c13afd453bfbf0c04f
Yudin Danil <danil.yudin.2014@list.ru>
2022-07-07 17:07:44 +0700

    Added available() implementation

--- src/Battleship.java
+++ src/Battleship.java
@@ -24,7 +24,51 @@ public class Battleship {
     }
 
     public boolean available(int x, int y, int deck, int rotation, int[][] battlefield) {
-        //todo реализовать метод available позднее до конца
+        if (rotation == 1) {
+            if (y + deck > battlefield.length) {
+                return false;
+            }
+        }
+        if (rotation == 2){
+            if (x + deck > battlefield[0].length){
+                return false;
+            }
+        }
+
+        while (deck!=0){
+            for (int i = 0; i < deck; i++) {
+                int xi = 0;
+                int yi = 0;
+                if (rotation == 1){
+                    yi = i;
+                } else{
+                    xi = i;
+                }
+
+                if (x + 1 + xi < battlefield.length && x + 1 + xi >= 0){
+                    if (battlefield[x + 1 + xi][y + yi]!=0){
+                        return false;
+                    }
+                }
+                if (x - 1 + xi < battlefield.length && x - 1 + xi >= 0){
+                    if (battlefield[x - 1 + xi][y + yi]!=0){
+                        return false;
+                    }
+                }
+                if (y + 1 + yi < battlefield.length && y + 1 + yi >= 0){
+                    if (battlefield[x + xi][y + 1 + yi]!=0){
+                        return false;
+                    }
+                }
+                if (y - 1 + yi < battlefield.length && y - 1 + yi >= 0){
+                    if (battlefield[x + xi][y - 1 + yi]!=0){
+                        return false;
+                    }
+                }
+            }
+            deck--;
+        }
         return true;
+    }
     }
 }
