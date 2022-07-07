a6ff61b35dd2c29e9fd87e39c7bf9bc3ef94e334
Yudin Danil <danil.yudin.2014@list.ru>
2022-07-07 12:31:24 +0700

    Added drawField trivial iomplementation

--- src/Battleship.java
+++ src/Battleship.java
@@ -2,4 +2,8 @@ public class Battleship {
     public Battleship() {}
 
 
+    public int drawField(int[][] battlefield) {
+        //todo реализовать метод drawField позднее до конца
+        return 10;
+    }
 }
